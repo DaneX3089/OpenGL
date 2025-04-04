76ce532521a9ffd58d4e4602b8d85ff033cb9230
chu pack <91909231+DaneX3089@users.noreply.github.com>
2025-04-04 23:34:48 +0400

    Everyting

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "libs.h"
+
+void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH) {
+	glViewport(0, 0, fbW, fbH);
+}
+
+bool loadShaders(GLuint& program) {
+	char infoLog[512];
+	GLint success;
+
+	std::string temp = "";
+	std::string src = "";
+
+	std::ifstream in_file;
+
+	in_file.open("vertex_core.glsl");
+}
+
+int main() {
+	glfwInit();
+
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGHT = 800;
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Window", NULL, NULL);
+	if (!window) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window); // Must be before glewInit()
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Error: GLEW failed to initialize\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	GLuint core_program;
+	loadShaders(core_program);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.1f, 0.0f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glFlush();
+	}
+	
+
+	glfwTerminate();
+	return 0;
+}
+
